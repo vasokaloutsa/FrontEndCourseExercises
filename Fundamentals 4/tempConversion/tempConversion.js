279b621fa5c47b6637136927e9921b127bc135c0
vasokaloutsa <v.kaloutsa@windowslive.com>
2020-11-22 21:49:07 +0200

    tempConversion Exercise complete

--- Fundamentals 4/tempConversion/tempConversion.js
+++ Fundamentals 4/tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const ftoc = function() {
+const ftoc = function(temperatureFarehneit) {
+  let temperatureCelsius;
+    temperatureCelsius=(temperatureFarehneit-32)*(5/9);
+    temperatureCelsius=Math.round(temperatureCelsius*10)/10;
+    return temperatureCelsius;
 
 }
 
-const ctof = function() {
+const ctof = function(temperatureCelsius) {
+  let temperatureFarehneit;
+  temperatureFarehneit=temperatureCelsius*9/5+32;
+  temperatureFarehneit=Math.round(temperatureFarehneit*10)/10;
+  return temperatureFarehneit;
 
 }
 
