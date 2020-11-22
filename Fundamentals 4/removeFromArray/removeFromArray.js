57a89aca311936b74442ba76b904693cc863a84d
vasokaloutsa <v.kaloutsa@windowslive.com>
2020-11-22 19:44:06 +0200

    removeFromArray exercise complete

--- Fundamentals 4/removeFromArray/removeFromArray.js
+++ Fundamentals 4/removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    let newArray=[];
+    let i=0;
+    
+    while (i<array.length){
 
+            if (array[i]!==args[0]&& array[i]!==args[1] && array[i]!==args[2] && array[i]!==args[3] ) {
+                
+                newArray.push(array[i]);   
+            }
+        
+         
+        i++;
+    }    
+
+    return newArray;
 }
 
 module.exports = removeFromArray
