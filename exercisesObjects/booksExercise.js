da55d18f2ab4f34e85e004d5c615be811b625f15
vasokaloutsa <v.kaloutsa@windowslive.com>
2021-01-14 13:14:26 +0200

    v.1 is ready(in v.2 I will add the optional step & style it better)

--- exercisesObjects/booksExercise.js
+++ exercisesObjects/booksExercise.js
@@ -24,29 +24,27 @@ function addBookToLibrary(title,author,pages,state){
 //render iterates through all array items and displays on window all the books of the list
 function render(){
   div.innerHTML = '';
+  div.innerHTML = "<th>Title</th> <th>Author</th> <th>Pages</th> <th>Read / Not read yet</th>"
+  div.style.cssText = "margin:0 auto;text-align:center; color:blue; font-size:1.2em";
   for (let i = 0; i < bookLibrary.length ; i++){
     let row = document.createElement('tr');
     div.appendChild(row);
     row.innerHTML =   `<td>${bookLibrary[i].title}</td>
                        <td>${bookLibrary[i].author}</td>
                        <td>${bookLibrary[i].pages}</td>
-                       <td><button id = "stateBtn${i}" >${bookLibrary[i].state}</button></td>
-                       <td><button id = "removebtn${i}" > Remove </button></td>`;
+                       <td><button id = "stateBtn${i}" class = "state-btn" >${bookLibrary[i].state}</button></td>
+                       <button id = "removebtn${i}" class = "remove-btn" > Remove </button>`;
 
     let removeButton = document.querySelector(`#removebtn${i}`);
     removeButton.addEventListener('click', ()=>{
-      removeBook(i);
-      render();
+        removeBook(i);
+        render();
     })
 
     let stateBtn = document.querySelector(`#stateBtn${i}`);
     stateBtn.addEventListener('click', ()=>{
-    
-    alert(bookLibrary[i].state);
-    toggleState(bookLibrary[i].state);
-    render();
-    alert(bookLibrary[i].state);
-    
+        toggleState(i);  
+        render(); 
     })
   }
   
@@ -57,43 +55,50 @@ function removeBook(index){
 }
 
 
-function toggleState(state){
-    if (state === 'not read yet'){
-       state = 'read';
-     
-    }else {
-      state = 'not read yet';
-    }
-    return alert(state);
+function toggleState(ind){
+  //alert(`do you want to change '${bookLibrary[ind].state}'?`);
+  if (window.confirm(`do you want to change '${bookLibrary[ind].state}'?`)){
+      if (bookLibrary[ind].state.toLowerCase() === 'not read yet'){
+        bookLibrary[ind].state = 'read';
+      
+      }else if(bookLibrary[ind].state.toLowerCase() === 'read') {
+        bookLibrary[ind].state = 'not read yet';
+      }
+      return bookLibrary[ind].state;
+  }else{
+    return;
+  }
+    
 }
 // DOM elements -------------------------
 
 // table element that displays all books
 const div = document.createElement('table');
 document.querySelector(".books-display").appendChild(div);
-div.innerHTML = "<th>Title</th> <th>Author</th> <th>Pages</th> <th>Read / Unread</th>"
+div.innerHTML = "<th>Title</th> <th>Author</th> <th>Pages</th> <th>Read / Not read yet</th>"
 div.style.cssText = "margin:0 auto;text-align:center; color:blue; font-size:1.2em";
 
 
 //new Book Button used to add new Books to list
 const newBookButton = document.querySelector('.buttons-style');
 newBookButton.addEventListener('click', ()=> {
-  let bookForm = document.createElement('form');
-  document.querySelector(".bform").appendChild(bookForm);
-  bookForm.innerHTML = `<input type = 'text' id = 'title' name = 'title' placeholder ='Book title'>
-                        <input type = 'text' id = 'author' name = 'author' placeholder ='Author name'>
-                        <input type = 'text' id = 'pages' name = 'pages' placeholder ='Number of pages'> 
-                        <input type = 'text' id = 'state' name = 'state'  placeholder ='Is it read or unread'>
-                        <button type = 'submit' id ='submit-book' >Submit book</button>`
-  
-                        
+    let bookForm = document.createElement('form');
+    document.querySelector(".bform").appendChild(bookForm);
+    bookForm.innerHTML = `<section class = "form-inputs"><input type = 'text' id = 'title' class = 'formEl' name = 'title' placeholder ='Book title'>
+                          <input type = 'text' id = 'author' class = 'formEl' name = 'author' placeholder ='Author name'>
+                          <input type = 'text' id = 'pages' class = 'formEl' name = 'pages' placeholder ='Number of pages'> 
+                          <input type = 'text' id = 'state' class = 'formEl' name = 'state'  placeholder ='Is it read or unread'>
+                          <button type = 'submit' id ='submit-book' >Submit book</button></section>`
+    bookForm.style.cssText = " display: block;   margin:0 auto;  margin-top:20px;  width:80%;  min-height: 40px;  border:1px solid green;"
+                          
   // addBookToLibrary(document.getElementById('title').value,document.getElementById('author').value,document.getElementById('pages').value,document.getElementById('state').value);
-  let  submitBtn = document.querySelector('#submit-book');
-  submitBtn.addEventListener('click' , ()=>{
-    addBookToLibrary(document.getElementById('title').value,document.getElementById('author').value,document.getElementById('pages').value,document.getElementById('state').value);
-    bookForm.innerHTML = '';
-    render();
-  })
+    let  submitBtn = document.querySelector('#submit-book');
+    submitBtn.addEventListener('click' , ()=>{
+      addBookToLibrary(document.getElementById('title').value,document.getElementById('author').value,document.getElementById('pages').value,document.getElementById('state').value);
+      bookForm.innerHTML = '';
+      bookForm.style.cssText = 'style:none';
+      render();
+    })
 })
 
 //Remove book button
@@ -101,7 +106,8 @@ document.querySelector('button').value
 
 
 
-addBookToLibrary('The Hobbit',' J.R.R. Tolkien', '295 pages', 'not read yet');
-addBookToLibrary('The Diary',' lkien', '885 pages', 'not read yet');
+addBookToLibrary('To Kill a Mockingbird',' Harper Lee', '295 pages', 'not read yet');
+addBookToLibrary('1984','George Orwell', '885 pages', 'read');
+addBookToLibrary('The Lord of the Rings','J.R.R. Tolkien', '895 pages', 'not read');
 render();
 console.log(bookLibrary);
